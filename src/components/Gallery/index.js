964bbd63a7edaa54493c5e7b7bfedf5d51de0ba0
rvanooyen <robert.vanooyen@gmail.com>
2021-12-15 14:20:59 -0600

    feature/gallery

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import PhotoList from '../PhotoList';
 import { capitalizeFirstLetter } from '../../utils/helpers';
 
-function Gallery({ currentCategory }) {
+function Gallery({currentCategory}) {
   const { name, description } = currentCategory;
   return (
     <section>
       <h1 data-testid="h1tag">{capitalizeFirstLetter(name)}</h1>
       <p>{description}</p>
-      <PhotoList  />
+      <PhotoList category={currentCategory.name}  />
     </section>
   );
 }
