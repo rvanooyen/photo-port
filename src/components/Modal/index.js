6c898726eee1f6d5af9fb5d385678b2e67764f9c
rvanooyen <robert.vanooyen@gmail.com>
2021-12-15 19:18:55 -0600

    feature/modal

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function Modal({currentPhoto}) {
+  const Modal = ({onClose, currentPhoto}) => {
     const {name, category, description, index} = currentPhoto;
     
     return (
@@ -8,15 +8,13 @@ function Modal({currentPhoto}) {
             <div className="modalContainer">
                 <h3 className="modalTitle">{name}</h3>
                 <img src={require(`../../assets/large/${category}/${index}.jpg`)} alt="current category" />
-                <p>
-                    {description}
-                </p>
-                <button type="button">
+                <p>{description}</p>
+                <button type="button" onClick={onClose}>
                     Close this modal
                 </button>
             </div>
         </div>
     );
-}
+};
 
 export default Modal;
--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import Modal from '../Modal';
 
 const PhotoList = ({ category }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentPhoto, setCurrentPhoto] = useState();
 
   const [photos] = useState([
     {
@@ -102,21 +104,17 @@ const PhotoList = ({ category }) => {
   ]);
 
   const currentPhotos = photos.filter((photo) => photo.category === category);
-  const [currentPhoto, setCurrentPhoto] = useState();
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
+  
   const toggleModal = (image, i) => {
       setCurrentPhoto({...image, index: i});
-      setIsModalOpen(true);
+      setIsModalOpen(!isModalOpen);
   }
 
   return (
     <div>
-      return (
-          <div>
-              {isModalOpen && <Modal currentPhoto={currentPhoto} />}
-          </div>
-      )
+      {isModalOpen && (
+        <Modal currentPhoto={currentPhoto} onClose={toggleModal}/>
+      )}
       <div className="flex-row">
         {currentPhotos.map((image, i) => (
           <img
